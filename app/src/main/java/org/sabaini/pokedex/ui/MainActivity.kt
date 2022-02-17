c1e0c7779168d5d7e4f42d7171bd7db461e69871
Paulo Sabaini <pasabaini@gmail.com>
2022-02-17 08:31:25 -0300

    adjustments to compile the app with the new compose version

--- app/src/main/java/org/sabaini/pokedex/ui/MainActivity.kt
+++ app/src/main/java/org/sabaini/pokedex/ui/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
-    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
